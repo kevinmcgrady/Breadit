11f2292905979ba04e71a6c49e26819fa138186a
Kev Mcgrady <kev@Kevs-MacBook-Pro.local>
2023-06-17 09:41:57 +0100

    feat/add-auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,40 @@
-import '@/styles/globals.css'
+import { cn } from '@/lib/utils';
+import '@/styles/globals.css';
+import { Inter } from 'next/font/google';
+import NavBar from '@/components/NavBar';
+import { Toaster } from '@/components/ui/Toaster';
 
 export const metadata = {
   title: 'Breadit',
   description: 'A Reddit clone built with Next.js and TypeScript.',
-}
+};
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({
   children,
+  authModel,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
+  authModel: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html
+      className={cn(
+        'bg-white text-slate-900 antialiased light',
+        inter.className,
+      )}
+      lang='en'
+    >
+      <body className='min-h-screen pt-12 bg-slate-50 antialiased'>
+        {/* @ts-expect-error server component */}
+        <NavBar />
+        {authModel}
+        <div className='container max-w-7xl mx-auth h-full pt-12'>
+          {children}
+        </div>
+        <Toaster />
+      </body>
     </html>
-  )
+  );
 }
---
+++ src/components/UserAuthForm.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { FC, useState } from 'react';
+import { signIn } from 'next-auth/react';
+import { Button } from './ui/Button';
+import { cn } from '@/lib/utils';
+import { Icons } from './Icons';
+import { useToast } from '@/hooks/use-toast';
+
+interface UserAuthFormProps extends React.HtmlHTMLAttributes<HTMLDivElement> {}
+
+const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { toast } = useToast();
+
+  const loginInWithGoogle = async () => {
+    setIsLoading(true);
+
+    try {
+      await signIn('google');
+    } catch (error) {
+      toast({
+        title: 'There was a problem',
+        description: 'There was an error loggin in with Google',
+        variant: 'destructive',
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div
+      onClick={loginInWithGoogle}
+      className={cn('flex justify-center', className)}
+      {...props}
+    >
+      <Button size='sm' className='w-full' isLoading={isLoading}>
+        {!isLoading && <Icons.google className='h-4 w-4 mr-2' />}
+        Google
+      </Button>
+    </div>
+  );
+};
+
+export default UserAuthForm;
